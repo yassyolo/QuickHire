1c46e1232e4220eb78ff9a1d3617c689c911cd53
Yoana Yotova <yoana.yotova03@gmail.com>
2025-04-29 18:21:57 +0300

    add eimal sender service to send emails through sendgrid

--- server/QuickHire/src/QuickHire.Infrastructure/Communication/EmailSenderService.cs
+++ server/QuickHire/src/QuickHire.Infrastructure/Communication/EmailSenderService.cs
@@ -1,5 +1,40 @@
-﻿namespace QuickHire.Infrastructure.Communication;
+﻿using Azure.Core;
+using Microsoft.Extensions.Options;
+using QuickHire.Application.Common.Interfaces.Services;
+using QuickHire.Application.Users.Models.Authentication;
+using QuickHire.Domain.Shared.Exceptions;
+using QuickHire.Infrastructure.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
-internal class EmailSenderService
+namespace QuickHire.Infrastructure.Communication;
+
+internal class EmailSenderService : IEmailSenderService
 {
+    private readonly SendGridOptions _sendGridOptions;
+
+    public EmailSenderService(IOptions<SendGridOptions> sendGridOptions)
+    {
+        _sendGridOptions = sendGridOptions.Value;
+    }
+
+    public async Task SendEmailAsync(EmailModel emailModel, CancellationToken cancellationToken = default)
+    {
+        var key = _sendGridOptions.ApiKey;
+        var client = new SendGridClient(key);
+        var from = new EmailAddress(_sendGridOptions.FromEmail, _sendGridOptions.FromName);
+        var subject = emailModel.Subject;
+        var to = new EmailAddress(emailModel.To, emailModel.To);
+        var msg = MailHelper.CreateSingleEmail(from, to, emailModel.Subject, plainTextContent: subject, htmlContent: subject);
+
+        var response = await client.SendEmailAsync(msg, cancellationToken);
+
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        {
+            throw new InternalServerErrorException("Failed to send email.", $"Failed to send email to {to}.");
+        }
+    }
 }
