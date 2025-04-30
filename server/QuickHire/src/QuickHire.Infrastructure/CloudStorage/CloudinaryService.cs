1a9aaacd1e81cfdf6fe30c26cef820400ad1c072
Yoana Yotova <yoana.yotova03@gmail.com>
2025-04-30 18:10:24 +0300

    Add cloudinary service for storing files in cloudinary

--- server/QuickHire/src/QuickHire.Infrastructure/CloudStorage/CloudinaryService.cs
+++ server/QuickHire/src/QuickHire.Infrastructure/CloudStorage/CloudinaryService.cs
@@ -1,5 +1,89 @@
 ﻿namespace QuickHire.Infrastructure.CloudStorage;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using QuickHire.Domain.Shared.Exceptions;
+using QuickHire.Infrastructure.Options;
+using static QuickHire.Infrastructure.CloudStorage.Constants.FolderOrganization;
+using static QuickHire.Infrastructure.CloudStorage.Constants;
+using QuickHire.Application.Common.Interfaces.Services;
 
-internal class CloudinaryService
+internal class CloudinaryService : ICloudinaryService
 {
+    private readonly Cloudinary _cloudinary;
+    private readonly CloudinaryOptions _cloudinaryOptions;
+
+    public CloudinaryService(IOptions<CloudinaryOptions> cloudinaryOptions)
+    {
+        _cloudinaryOptions = cloudinaryOptions.Value;
+        var account = new Account(_cloudinaryOptions.CloudName, _cloudinaryOptions.ApiKey, _cloudinaryOptions.ApiSecret);
+        _cloudinary = new Cloudinary(account);
+    }
+
+    public string UploadFile(IFormFile file)
+    {
+        if(file == null || file.Length == 0)
+        {
+            throw new BadRequestException("File is null or empty", $"File {nameof(file)} not existing.");
+        }
+
+        if (file.Length > 10 * 1024 * 1024)
+        {
+            throw new BadRequestException("File size exceeded", $"File size {file.Length} exceeds the limit of 10MB.");
+        }
+
+        var extension = Path.GetExtension(file.FileName).ToLower();
+
+        string mimeType = extension switch
+        {
+            SupportedExtensions.Jpg or SupportedExtensions.Jpeg => SupportedMimeTypes.Jpg,
+            SupportedExtensions.Png => SupportedMimeTypes.Png,
+            SupportedExtensions.Pdf => SupportedMimeTypes.Pdf,
+            SupportedExtensions.Mp4 => SupportedMimeTypes.Mp4,
+            _ => throw new BadRequestException("Unsupported file type", $"File type {extension} is not supported.")
+        };
+
+        if(mimeType == "image/jpeg")
+        {
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),
+                Transformation = new Transformation().Width(500).Height(500).Crop("limit").Quality("auto"),
+                Format = extension.Substring(1),
+                Overwrite = true,
+                PublicId = Image + Guid.NewGuid().ToString(),
+            };
+
+            var uploadUri = _cloudinary.Upload(uploadParams);
+            return uploadUri?.SecureUri?.ToString();
+        }
+        else if (mimeType == "application/pdf")
+        {
+            var rawUploadParams = new RawUploadParams
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),   
+                PublicId = Document + Guid.NewGuid().ToString(),
+            };
+
+            var uploadResult = _cloudinary.Upload(rawUploadParams);
+            return uploadResult?.SecureUrl?.ToString();
+        }
+        else if (mimeType == "video/mp4")
+        {
+            var videoUploadParams = new VideoUploadParams
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),
+                Transformation = new Transformation().Width(500).Height(500).Crop("limit").Quality("auto"),
+                Format = extension.Substring(1),
+                Overwrite = true,
+                PublicId = FolderOrganization.Video + Guid.NewGuid().ToString(),
+            };
+
+            var uploadResult = _cloudinary.Upload(videoUploadParams);
+            return uploadResult?.SecureUrl?.ToString();
+        }
+
+        return string.Empty;
+    }
 }
---
+++ server/QuickHire/src/QuickHire.Infrastructure/CloudStorage/Constants.cs
@@ -0,0 +1,34 @@
+﻿namespace QuickHire.Infrastructure.CloudStorage;
+
+internal static class Constants
+{
+    internal static class SupportedMimeTypes
+    {
+        public const string Pdf = "application/pdf";
+        public const string Jpg = "image/jpeg";
+        public const string Jpeg = "image/jpeg";
+        public const string Png = "image/png";
+        public const string Mp4 = "video/mp4";
+    }
+
+    internal static class SupportedExtensions
+    {
+        public const string Pdf = ".pdf";
+        public const string Jpg = ".jpg";
+        public const string Jpeg = ".jpeg";
+        public const string Png = ".png";
+        public const string Mp4 = ".mp4";
+    }
+
+    internal static class SupportedFileSize
+    {
+        public const int MaxSizeInBytes = 10 * 1024 * 1024; 
+    }
+
+    internal static class FolderOrganization
+    {
+        public const string Image = "images/";
+        public const string Document = "documents/";
+        public const string Video = "videos/";
+    }
+}
