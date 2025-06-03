9eefd179b389a972ad000d9f92afe69dd5aead8c
Yoana Yotova <yoana.yotova03@gmail.com>
2025-06-03 18:37:50 +0300

    add soundex db function

---
+++ server/QuickHire/src/QuickHire.Infrastructure/Persistence/EFHelpers/SqlFunctions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuickHire.Infrastructure.Persistence.EFHelpers;
+
+public static class SqlFunctions
+{
+    [DbFunction("SOUNDEX", IsBuiltIn = true)]
+    public static string Soundex(string input)
+    {       
+        throw new NotSupportedException();
+    }
+}
